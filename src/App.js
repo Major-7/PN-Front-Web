51bc0f4a5da8fcd695e1a4a53a0e3b913335ece1
root <keshavaarav22@gmail.com>
2020-09-21 10:56:27 +0530

    Reset Password Route Added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/navbar";
-import Forgot from "./components/forgotPass"
+import Forgot from "./components/forgotPass";
+import Reset from "./components/Reset"
 
 function App() {
 	return (
@@ -15,6 +16,7 @@ function App() {
 					<Route exact path="/login" component={Login}></Route>
 					<Route exact path="/register" component={Register}></Route>
 					<Route exact path="/forgotPassword" component={Forgot}></Route>
+					<Route exact path="/resetPassword" component={Reset}></Route>
 
 					<Route path="/" component={Login}></Route>
 				</Switch>
