6f5e15df67fafefadcc3a7692374b97938ec76d9
Dhawal Singh Panwar <dsp9107@outlook.com>
2020-12-10 10:30:03 +0530

    new filters added

--- src/App.js
+++ src/App.js
@@ -1,81 +1,140 @@
 import React from "react";
 import "./App.css";
 import { useState } from "react";
-import { TextField, Button, makeStyles, Container } from "@material-ui/core";
+import { TextField, Button, makeStyles, Container, Select, MenuItem, FormHelperText, FormControl, InputLabel } from "@material-ui/core";
 import Search from "@material-ui/icons/Search";
 
-const useStyles = makeStyles({
-  container: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
+const useStyles = makeStyles(theme => ({
+	container: {
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+  },
+  
+  inputField: {
+    width: "200px",
+    margin: "8px 0"
   },
 
-  btn: { marginLeft: 20 },
-});
+	btn: {
+    margin: "18px 0"
+  },
+
+  formControl: {
+    margin: theme.spacing(1),
+    width: "200px",
+    textAlign: "left"
+  },
+
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 function App() {
-  const [queryCGPA, setQueryCGPA] = useState("");
-  const [students, setStudents] = useState([]);
-  const [error, setError] = useState(null);
+	const [queryCGPA, setQueryCGPA] = useState("");
+	const [queryYEAR, setQueryYEAR] = useState("");
+	const [queryBRANCH, setQueryBRANCH] = useState("");
+	const [querySPEC, setQuerySPEC] = useState("");
+	const [students, setStudents] = useState([]);
+	const [error, setError] = useState(null);
 
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const apiQuery = () => {
-    fetch(
-      `https://asia-east2-ait-application-suite.cloudfunctions.net/getEligible?cgpa=${queryCGPA}`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json);
-        if (json.msg === "success") setStudents(json.data);
-      })
-      .catch((err) => {
-        console.log("Error in fetching results: ", err);
-        setError(err);
-        alert(error);
-      });
-  };
+	const apiQuery = () => {
+		if (!queryCGPA && !queryYEAR && !queryBRANCH && !querySPEC) {
+			alert("select atleast one filter");
+			return;
+    }
+    window.location.assign(`http://localhost:5001/ait-application-suite/asia-east2/getEligibleP1B?cgpa=${queryCGPA}&year=${queryYEAR}&branch=${queryBRANCH}&spec=${querySPEC}`);
+	};
 
-  const inputHandler = (evt) => {
-    setQueryCGPA(evt.target.value);
-  };
+	const handleCgpaChange = (evt) => {
+		setQueryCGPA(evt.target.value);
+	};
 
-  const submitHandler = (evt) => {
-    evt.preventDefault();
-    apiQuery();
-  };
+  const handleYearChange = (evt) => {
+		setQueryYEAR(evt.target.value);
+	};
+  
+  const handleBranchChange = (evt) => {
+    setQueryBRANCH(evt.target.value);
+	};
+  
+  const handleSpecChange = (evt) => {
+		setQuerySPEC(evt.target.value);
+	};
 
-  return (
-    <div className="App">
-      <h1>Platinum Notifier</h1>
-      <form onSubmit={submitHandler}>
-        <Container className={classes.container}>
-          <TextField
-            id="cgpa-input"
-            label="CGPA cutoff"
-            value={queryCGPA}
-            onChange={inputHandler}
-          />
-          <Button
-            type="submit"
-            color="primary"
-            className={classes.btn}
-            variant="contained"
-            endIcon={<Search />}
-          >
-            Search
-          </Button>
-        </Container>
-      </form>
-      {students.email && (
-        <p>
-          <strong>Email : </strong> {students.email}
-        </p>
-      )}
-    </div>
-  );
+	const submitHandler = (evt) => {
+		evt.preventDefault();
+		apiQuery();
+	};
+
+	return (
+		<div className="App">
+			<h1>Platinum Notifier</h1>
+			<form onSubmit={submitHandler}>
+				<Container className={classes.container}>
+					<TextField
+            className={classes.inputField}
+						id="cgpa-input"
+						label="CGPA cutoff"
+						value={queryCGPA}
+						onChange={handleCgpaChange}
+					/>
+					<TextField
+            className={classes.inputField}
+						id="year-input"
+						label="Year of Grad."
+						value={queryYEAR}
+						onChange={handleYearChange}
+					/>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="branch-select-label">Branch</InputLabel>
+            <Select
+              labelId="branch-select-label"
+              id="branch-select"
+              value={queryBRANCH}
+              onChange={handleBranchChange}
+            >
+              <MenuItem value={"cse"}>CSE</MenuItem>
+              <MenuItem value={"cse_ibm"}>CSE IBM</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="spec-select-label">Specialization</InputLabel>
+            <Select
+              labelId="spec-select-label"
+              id="spec-select"
+              value={querySPEC}
+              onChange={handleSpecChange}
+            >
+              <MenuItem value={"is"}>IS</MenuItem>
+              <MenuItem value={"bd"}>BD</MenuItem>
+              <MenuItem value={"cc"}>CC</MenuItem>
+              <MenuItem value={"iot"}>IoT</MenuItem>
+              <MenuItem value={"aiml"}>AIML</MenuItem>
+            </Select>
+          </FormControl>
+					<Button
+						type="submit"
+						color="primary"
+						className={classes.btn}
+						variant="contained"
+						endIcon={<Search />}
+					>
+						Download
+					</Button>
+				</Container>
+			</form>
+			{students.email && (
+				<p>
+					<strong>Email : </strong> {students.email}
+				</p>
+			)}
+		</div>
+	);
 }
 
 export default App;
